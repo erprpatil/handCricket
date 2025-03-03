c2a5d5980b9c459bcbf277e5dd667ac04c5d3762
erprpatil <121144148+erprpatil@users.noreply.github.com>
2025-03-02 20:02:12 -0500

    added & tested play game function

--- cricket.js
+++ cricket.js
@@ -1,19 +1,52 @@
-let score = 0;
-
+let runs;
+let computerMove;
 let scoreboard = [];
 
 const run1Button = document.querySelector(".js-1-run");
 
 run1Button.addEventListener("click", () => {
-  console.log("1 Run");
-  score = 1;
-  addScore();
+  runs = 1;
+  console.log(`${runs}Run`);
+  playComputerMove();
+  throwBall(runs, computerMove);
+
   console.log(scoreboard);
 });
 
 function addScore(){
-    scoreboard.push(score);
+    scoreboard.push(runs);
 }
 
+
+
 function playComputerMove (){
+
+  let randNum = Math.floor(Math.random() * 6 + 1);
+
+  if(randNum === 5){ //5 is no ball
+    computerMove = 0;
+  } else{
+    computerMove = randNum;
+  }
+  console.log(`Computer move is: ${computerMove}`);
+  return computerMove;
 }
+
+function throwBall(runs, computerMove){ 
+
+  if (runs === computerMove){
+    console.log ('You are Out!!!!');
+    runs = 'Wkt';
+    scoreboard.push(runs);
+  }else{
+    scoreboard.push(runs);
+  }
+}
+
+
+/*
+Next steps-
+For no ball, score 1.
+JSON save score
+
+*/
